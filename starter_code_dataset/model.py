1f8c6b0a451a1262d28f83f6ff8ef96bf6375920
Vineeth <saivineethkumar@gmail.com>
2023-05-09 23:06:56 -0400

    adding model code

--- starter_code_dataset/model.py
+++ starter_code_dataset/model.py
@@ -15,9 +15,11 @@ class MaskedCNN(nn.Conv2d):
         _, _, height, width = self.weight.size()
         self.mask.fill_(1)
         if mask_type == 'A':
+            # center too is zero here
             self.mask[:, :, height//2, width//2:] = 0
             self.mask[:, :, height//2+1:, :] = 0
         else:
+            # center is not zero here
             self.mask[:, :, height//2, width//2+1:] = 0
             self.mask[:, :, height//2+1:, :] = 0
 
@@ -35,11 +37,43 @@ class PixelCNN(nn.Module):
         super(PixelCNN, self).__init__()
 
         # WRITE CODE HERE TO IMPLEMENT THE MODEL STRUCTURE
+        self.mask_conv1 = MaskedCNN(in_channels=1, out_channels=16, kernel_size=3, stride=1, 
+                                    padding_mode='reflect', padding=3, dilation=3, mask_type='A')
+        self.batch_norm1 = nn.BatchNorm2d(16)
+        self.leaky_relu1 = nn.LeakyReLU(negative_slope=0.001)
 
-        self.conv = nn.Conv2d(in_channels=1, out_channels=1, kernel_size=1)
+        self.mask_conv2 = MaskedCNN(in_channels=16, out_channels=16, kernel_size=3, stride=1, 
+                                    padding_mode='reflect', padding=3, dilation=3, mask_type='B')
+        self.batch_norm2 = nn.BatchNorm2d(16)
+        self.leaky_relu2 = nn.LeakyReLU(negative_slope=0.001)
+
+
+        self.mask_conv3 = MaskedCNN(in_channels=16, out_channels=16, kernel_size=3, stride=1, 
+                                    padding_mode='reflect', padding=3, dilation=3, mask_type='B')
+        self.batch_norm3 = nn.BatchNorm2d(16)
+        self.leaky_relu3 = nn.LeakyReLU(negative_slope=0.001)
+
+        self.conv4 = nn.Conv2d(in_channels=16, out_channels=1, kernel_size=1)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
 
         # WRITE CODE HERE TO IMPLEMENT THE FORWARD PASS
-        return self.conv(self.sigmoid(x))
+        x = self.mask_conv1(x)
+        x = self.batch_norm1(x)
+        x = self.leaky_relu1(x)
+        
+        x = self.mask_conv2(x)
+        x = self.batch_norm2(x)
+        x = self.leaky_relu2(x)
+        
+        x = self.mask_conv3(x)
+        x = self.batch_norm3(x)
+        x = self.leaky_relu3(x)
+
+        x = self.conv4(x)
+        x = self.sigmoid(x)
+        
+        return x
+
+
